2b6b9e30bdb4fee0899a2f8dc35a702de4039d40
chowbeizi <beatoperator@yahoo.com>
2026-02-13 14:25:36 +0100

    archive filters system

---
+++ src/config/tags.ts
@@ -0,0 +1,14 @@
+export const TAGS = {
+  esej: {
+    label: "ESEJ",
+    color: "#0057ff",
+  },
+  kritika: {
+    label: "KRITIKA",
+    color: "#c5161d",
+  },
+  film: {
+    label: "FILM",
+    color: "#0a8f3c",
+  },
+};
