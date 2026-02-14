12382db246c3c291f28e5b9e59009660955cd1e2
chowbeizi <beatoperator@yahoo.com>
2026-02-14 16:20:43 +0100

    fresh install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://filmmort1.pages.dev',  // ← STAVI CLOUDFLARE URL
-  output: 'static',  // ← STATIC, ne server
+  site: 'https://filmmort1.pages.dev',
+  output: 'static',  // Ostaje static
+  // POTPUNO MAKNUT ADAPTER
   integrations: [mdx(), sitemap()],
-  
 });
