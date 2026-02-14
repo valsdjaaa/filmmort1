78ad6892efda936b66bbb6e68b8e0ecbd270b912
chowbeizi <beatoperator@yahoo.com>
2026-02-14 04:06:22 +0100

    admin panel za tagove

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://example.com',
+  output: 'hybrid',  // ← DODAJ OVO
+  adapter: netlify(), // ← DODAJ OVO
   integrations: [mdx(), sitemap()],
 });
