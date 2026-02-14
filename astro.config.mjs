d20ef561f49aa2dffb5025842080c9e840e639a9
chowbeizi <beatoperator@yahoo.com>
2026-02-14 15:44:51 +0100

    fix: set output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  site: 'https://example.com',
-  output: 'server',  // ← PROMENI U server
-  adapter: netlify(),
+  site: 'https://filmmort1.pages.dev',  // ← STAVI CLOUDFLARE URL
+  output: 'static',  // ← STATIC, ne server
   integrations: [mdx(), sitemap()],
 });
